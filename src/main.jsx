63cefedfb6c10ed10e12580ac7838db1c786fcc2
Rishikeshsanin <141367936+Rishikeshsanin@users.noreply.github.com>
2026-09-17 00:26:36 +0530

    release: ship Closeby location and scrolling fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import './styles.css'
 import App from './App'
-import LiveApp from './LiveApp'
+import LiveAppV2 from './LiveAppV2'
 
 const demoMode = new URLSearchParams(window.location.search).get('demo') === '1'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {demoMode ? <App /> : <LiveApp />}
+    {demoMode ? <App /> : <LiveAppV2 />}
   </React.StrictMode>,
 )
