04313ead9301b81fc89b207bf655351914d2d082
Rishikeshsanin <141367936+Rishikeshsanin@users.noreply.github.com>
2026-09-16 23:38:57 +0530

    feat: stage live Closeby client behind preview flag

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import './styles.css'
 import App from './App'
+import LiveApp from './LiveApp'
+
+const livePreview = new URLSearchParams(window.location.search).get('live') === '1'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {livePreview ? <LiveApp /> : <App />}
   </React.StrictMode>,
 )
